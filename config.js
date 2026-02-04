2fb85a53df9d5fe882dfe0a449c8c5bd8c7134a0
eruuamamiya <papapap803@gmail.com>
2026-02-04 22:07:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,11 @@
 // Ganti URL di bawah ini dengan Link Cloudflare Tunnel kamu yang aktif.
 // Contoh: "https://nama-tunnel-kamu.trycloudflare.com"
 
-const API_BASE_URL = "https://our-solaris-comfort-periods.trycloudflare.com"; 
+const API_BASE_URL = "https://reduced-confidence-muscle-baseline.trycloudflare.com/otakudesu"; 
 
 // (Biarkan baris ini, untuk mencegah error jika variabel belum ada)
 if (typeof API_BASE_URL === 'undefined') {
     console.warn('API_BASE_URL belum diatur di config.js');
 }
+
 
